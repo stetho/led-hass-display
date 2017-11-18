763b3211ac6024a0e020ec3fbeae52c0f26c50a6
Steve Thompson <steve@stetho.co.uk>
2017-11-18 20:25:41 +0000

    Added a feature to display BBC News Headline when it changes

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import time
 import datetime
 import argparse
 import paho.mqtt.client as paho
+import feedparser
 
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
@@ -14,17 +15,25 @@ from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=-90, rotate=2)
 device.contrast(1)
+
 def on_three(mqttc, obj, mqmsg):
     print(mqmsg.topic+" "+str(mqmsg.qos)+" "+str(mqmsg.payload))
     device.contrast(240)
     for i in range(3):
         show_message(device, str(mqmsg.payload), fill="white", font=proportional(LCD_FONT))
     device.contrast(1)
+
 def on_message(mqttc, obj, mqmsg):
     print(mqmsg.topic+" "+str(mqmsg.qos)+" "+str(mqmsg.payload))
     device.contrast(240)
     show_message(device, str(mqmsg.payload), fill="white", font=proportional(LCD_FONT))
     device.contrast(1)
+
+def on_unimportant(mqttc, obj, mqmsg):
+    print(mqmsg.topic+" "+str(mqmsg.qos)+" "+str(mqmsg.payload))
+    show_message(device, str(mqmsg.payload), fill="white", font=proportional(LCD_FONT))
+
+
 
 mqtt = paho.Client()
 #mqtt.on_message = on_message
@@ -32,21 +41,40 @@ mqtt.connect("192.168.101.15", 1883)
 mqtt.subscribe("lhd/#")
 mqtt.message_callback_add("lhd/message", on_message)
 mqtt.message_callback_add("lhd/three", on_three)
+mqtt.message_callback_add("lhd/unimportant", on_unimportant)
+
+oldheadline = ''
+url = 'http://feeds.bbci.co.uk/news/rss.xml?edition=uk'
+
 print "* Looping"
 #mqtt.loop_start()
 
 while 1:
     now = datetime.datetime.now()
     secs = int(time.time()-(int(time.time()/60)*60))
-#    if secs % 2 == 0: #even 
-#        msg = now.strftime("%H.%M")
-#    else: #odd
-#        msg = now.strftime("%H:%M")
 
-    msg = now.strftime("%H:%M:%S")
+    if (secs == 0 or oldheadline == ''):
+        feed = feedparser.parse(url)
+        chl = feed.entries[0]['title']
+        if (chl != oldheadline):
+            oldheadline = chl
+            with canvas(device) as draw:
+                text(draw, (0,0), "BBC NEWS", fill="white", font=proportional(LCD_FONT))
+                time.sleep (1)
+                text(draw, (0,0), "", fill="white", font=proportional(LCD_FONT))
+                time.sleep (1)
+                text(draw, (0,0), "BBC NEWS", fill="white", font=proportional(LCD_FONT))
+                time.sleep (1)
+                text(draw, (0,0), "", fill="white", font=proportional(LCD_FONT))
+                time.sleep (1)
+                text(draw, (0,0), "BBC NEWS", fill="white", font=proportional(LCD_FONT))
+                time.sleep (1)
+            show_message(device, chl, fill="white", font=proportional(LCD_FONT))
+
+    msg = now.strftime("%H:%M")
     mqtt.loop(0.5)
     with canvas(device) as draw:
-        text(draw, (0,0), msg, fill="white", font=proportional(TINY_FONT))
+        text(draw, (0,0), msg, fill="white", font=proportional(LCD_FONT))
 
 
 
